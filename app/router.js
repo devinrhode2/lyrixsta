2ba70a924b5b3eadced621001f2324ad5438551c
Devin Rhode <devinrhode2@gmail.com>
2016-07-16 11:37:33 -0700

    ember-redirect's: synth(s) -> instrument(s)

--- app/router.js
+++ app/router.js
@@ -2,7 +2,11 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  redirects: {
+    synth     : 'instrument',
+    synths    : 'instruments'
+  }
 });
 
 Router.map(function() {
