6abf980efd9cfb6fa144dc55761790df83caed5c
Devin Rhode <devinrhode2@gmail.com>
2016-07-16 16:56:36 -0700

    Figuring out #hbss app template+controller+appTheme.js

---
+++ app/controllers/appTheme.js
@@ -0,0 +1,7 @@
+//import framework from 'hbss-kit'
+//put your apps theme here!
+
+export default {
+    'app-main-trim': '222',
+    'app-nav-height': '24'
+}
---
+++ app/controllers/application.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import appTheme from './appTheme'; //would eventually be application/theme.hbss or 
+// './theme.hbs' or 'appTheme.hbss' or 'appTheme.js' could do appTheme({...overrides})
+//import mainTheme from './mainTheme.js';
+
+//themable at every level, app, engine, 
+
+export default Ember.Controller.extend({
+    style_vars: appTheme
+});
